72787e8cccb87c3878c5c8477d9ebd07d0dd956a
praveenbot12 <praveensharmapk12@gmail.com>
2025-01-05 09:23:25 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY YouTube BuZz
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7785961256:AAH-ZvbTRJAGifL0XFByle4oECQ-yq8GfOg")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("BUZZ_96K")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5997540330")
+
+
+
+
+
+
+
+  
